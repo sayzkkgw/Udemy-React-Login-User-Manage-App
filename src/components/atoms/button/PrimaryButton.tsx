b443ba69e364597561f196b9aac102154fd0f5c8
Seiji KK <seijikakogawa@Seijis-MacBook-Air.local>
2022-09-29 22:13:29 +0900

    style(code  style): formatting style

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -11,6 +11,13 @@ type Props = {
 export const PrimaryButton: FC<Props> = memo((props) => {
     const { children, disabled = false, loading = false, onClick } = props;
     return (
-        <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }} isDisabled={disabled || loading} isLoading={loading} onClick={onClick}>{children}</Button>
+        <Button
+            bg="teal.400"
+            color="white"
+            _hover={{ opacity: 0.8 }}
+            isDisabled={disabled || loading}
+            isLoading={loading}
+            onClick={onClick}>{children}
+        </Button>
     )
 })

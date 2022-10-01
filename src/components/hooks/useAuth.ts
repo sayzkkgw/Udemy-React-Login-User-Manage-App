6d603632d1e5639c4b40415b3ae3703c0dae3811
Seiji KK <sayzkkgw@gmail.com>
2022-10-02 01:55:37 +0900

    feat(login toast): create login toast popped up when login

--- src/components/hooks/useAuth.ts
+++ src/components/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import { useCallback, useState } from "react"
 import axios from 'axios';
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
     const navigate = useNavigate();
+    const { showMessage } = useMessage();
     const [loading, setLoading] = useState(false);
     const login = useCallback((id: string) => {
         setLoading(true);
@@ -12,13 +14,17 @@ export const useAuth = () => {
             .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then((res) => {
                 if (res.data) {
+                    showMessage({ title: "Login suceeded!", status: "success" })
                     navigate("/home")
                 } else {
-                    alert("No user found");
+                    // alert("No user found");
+                    showMessage({ title: "No user found...", status: "error" })
                 }
             })
-            .catch(() => alert("Can't login. Please retry."))
+            .catch(() => {
+                showMessage({ title: "Login failed.", status: "error" })
+            })
             .finally(() => setLoading(false));
-    }, [navigate])
+    }, [navigate, showMessage])
     return { login, loading };
 }

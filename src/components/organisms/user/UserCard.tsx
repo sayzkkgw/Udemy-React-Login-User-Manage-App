be64ba6dd82013825327900c682e4675c466b2e8
Seiji KK <sayzkkgw@gmail.com>
2022-10-03 10:43:36 +0900

    feat(user card and user list): user card component and apply it to the user management page

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,28 @@
+import { FC, memo } from "react";
+import { Box, Stack, Image, Text } from '@chakra-ui/react';
+
+type Props = {
+    imageUrl: string;
+    userName: string;
+    fullName: string;
+}
+
+export const UserCard: FC<Props> = memo((props) => {
+    const { imageUrl, userName, fullName } = props;
+    return (
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="lg" p={4} _hover={{ cursor: "pointer", opacity: 0.8 }}>
+            <Stack textAlign="center">
+                <Image
+                    m="auto"
+                    alt="Profile"
+                    borderRadius="full"
+                    boxSize="160px"
+                    src={imageUrl}
+                ></Image>
+                <Text fontSize="lg" fontWeight="bold">{userName}</Text>
+                <Text fontSize="sm" color="gray">{fullName}</Text>
+            </Stack>
+        </Box>
+
+    )
+})
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,15 @@
-import { FC, memo } from "react"
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
     return (
-        <p>UserManagement</p>
+        <Wrap p={{ base: 4, md: 10 }}>
+            <WrapItem>
+                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
+                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
+                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
+            </WrapItem>
+        </Wrap >
     )
 })

1dee03ea3919fb9c7304eba48523532aaf8b51e4
Seiji KK <sayzkkgw@gmail.com>
2022-10-03 11:31:31 +0900

    feat(user management list): implement 'get user management list'

---
+++ src/components/hooks/useAllUsers.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useCallback, useState } from "react"
+import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+    const { showMessage } = useMessage();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [users, setUsers] = useState<Array<User>>([]);
+
+    const getUsers = useCallback(() => {
+        setLoading(true)
+        axios
+            .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+            .then((res) => setUsers(res.data))
+            .catch(() => showMessage({ title: "Failed to get user informaiton", status: "error" }))
+            .finally(() => setLoading(false));
+    }, [showMessage]);
+    return { getUsers, loading, users }
+}
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,27 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { useAllUsers } from "../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers();
+    useEffect(() => getUsers(), [getUsers]);
     return (
-        <Wrap p={{ base: 4, md: 10 }}>
-            <WrapItem>
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="Smith" fullName="John Smith" />
-            </WrapItem>
-        </Wrap >
+        <>
+            {
+                loading ?
+                    <Center h="100vh">
+                        <Spinner />
+                    </Center>
+                    :
+                    <Wrap p={{ base: 4, md: 10 }}>
+                        {users.map((user) => (
+                            < WrapItem key={user.id} mx="auto">
+                                <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+                            </WrapItem>
+                        ))}
+                    </Wrap >
+            }
+        </>
     )
 })
